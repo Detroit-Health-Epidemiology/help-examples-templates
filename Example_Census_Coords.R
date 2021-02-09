bda464fe9fe491e5150b2f744255904876e4e608
bell-samantha <35202459+bell-samantha@users.noreply.github.com>
2021-02-09 11:55:27 -0500

    Geocode using census geocoder and addresses

--- Example_Census_Coords.R
+++ Example_Census_Coords.R
@@ -1,40 +1,28 @@
 library(censusxy)
+library(sf)
 
+Data <- [Your Data Here]
 
-# Add coordinates to the street addresses, Takes a long time!
-1- Takes list of positive addresses which are complete, and not PO box or a lab
-2- Compares to census data and creates lattitude and longitude
-3- Joins lattitude and longitude back into the complete dataframe, matching to appropriate row
-```{r findCoords}
 # Make a subset with investigation ID and address for positive cases
-posOnly <- covidData[covidData$Case_Status == "Confirmed",]
-add <- as.data.frame(cbind(posOnly$Street_Address, posOnly$City, posOnly$State, posOnly$Simple_Investigation_Zip, posOnly$InvestigationID))
+posOnly <- Data[Data$Case_Status == "Confirmed",]
+add <- posOnly %>% select(Street_Address, City, State, Zip, InvestigationID)
 
-# Take out rows with missing pieces, or not Detroit, MI
-add <- add[!is.na(add[,1])&add[,1]!="",]
-add <- add[add[,2]=="Detroit"|add[,2] =="detroit"|add[,2] =="DETROIT"|add[,2] =="Detroit City",]
-add <- add[add[,3]=="Michigan"|add[,3]=="MI",]
-add <- add[!is.na(add[,4])&add[,4]!="",]
+# Remove addresses with NA values (NAs make geocoder very slow and less accurate)
+add <- add[!is.na(add[1]),]
+add <- add[!is.na(add[2]),]
+add <- add[!is.na(add[3]),]
+add <- add[!is.na(add[4]),]
 
-# Remove PO Boxes and 400 Mack (WSU) and 4201 Antoine (DMC) from addresses to be given coords
-# Remove Jefferson Ave E Nursing home testing site
-add <- add[!grepl("(400 Mack|PO Box|P O BOX|P.O. BOX|4201 ST ANTOINE ST|4201 ST ANTOINE ST STE A|Unknown|homeless)", add$V1, ignore.case = TRUE),]
+# Remove PO Boxes and other desired addresses to be excluded
+exclusion_Addresses <- "(^100 MACK|^400 MACK|PO Box|P O BOX|P.O. BOX|Unknown|homeless|^2799 (W|WEST)*.*GRAND B|^22101 MOROSS R|^4646 JOHN R R|1 ford pl|one energy pl|1 energy pl|^5635 (W|WEST)*.*FORT S|^2 WOODWARD|^500 GRISWOLD S|^4201 (ST|SAINT) ANTOINE S|^4646 JOHN R S|^570 CLINTON S|^3456 (W|WEST)*.*VERNOR H|^6071 (W|WEST)*.*OUTER DR|^16238 (W|WEST)*.*WARREN A|^600 (E|EAST)*.*LAFAYETTE B|^23 (E|EAST)*.* ADAMS AVE|^4880 LAWNDALE S|^17600 RYAN R)"
+add <- add[!grepl(exclusion_Addresses, add$Street_Address, ignore.case = TRUE),]
 
-# THE ACTION #
 # Get coords for street addresses by pinging the USA census data
-addresses <- cxy_geocode(add, address = V1, city = V2, state = V3, zip = V4,
-                         style = "minimal", output = "sf")
+addresses <- cxy_geocode(add, street = "Street_Address", city = "City", state = "State", output = "simple")
 
-# For each address, separate out lat and long coords
-Lattitude <- c()
-Longitude <- c()
-for(i in 1:dim(addresses)[1]){
-  Longitude[i] <- addresses$geometry[[i]][1]
-  Lattitude[i] <- addresses$geometry[[i]][2]
-}
-
-# bind latittude, longitude, and investigation ID into a dataframe, then join back into covidData
-coords <- as.data.frame(cbind(as.character(addresses$V5), Lattitude, Longitude))
-covidData$InvestigationID <- as.character(covidData$InvestigationID)
-coords$V1 <- as.character(coords$V1)
-covidData <- left_join(covidData, coords, by = c("InvestigationID"="V1"))
+# bind latittude, longitude, and investigation ID into a dataframe, then join back into finalData
+coords <- addresses %>% select(InvestigationID, cxy_lat, cxy_lon) %>% rename(
+  Lattitude = cxy_lat,
+  Longitude = cxy_lon
+)
+Data <- left_join(Data, coords, by = "InvestigationID")
